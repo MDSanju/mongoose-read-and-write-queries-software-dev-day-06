0c0ade71c2e92b4c3eea6f44ab5bb8029a4f6fa8
MDSanju <mdsanju387@gmail.com>
2023-05-25 23:14:51 -0700

    create interface, schema, model and insert test data to database

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,132 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { Schema, model } from "mongoose";
 
 // Create an instance of Express
 const app: Application = express();
 
+// Middlewares
 
-// Middleware
+// Use cors
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
+// Parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World!');
-    next();
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  /*
+    step 1: Interface
+    step 2: Schema
+    step 3: Model
+    step 4: Database Query
+  */
+
+  // step 1 (create an interface)
+  interface IUser {
+    id: string;
+    role: 'student';
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: 'male' | 'female';
+    email?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // step 2 (create a schema using the interface)
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    role: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true
+      },
+      middleName: {
+        type: String
+      },
+      lastName: {
+        type: String,
+        required: true
+      },
+    },
+    dateOfBirth: {
+      type: String
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female']
+    },
+    email: {
+      type: String
+    },
+    contactNo: {
+      type: String,
+      required: true
+    },
+    emergencyContactNo: {
+      type: String,
+      required: true
+    },
+    presentAddress: {
+      type: String,
+      required: true
+    },
+    permanentAddress: {
+      type: String,
+      required: true
+    },
+  });
+
+  // step 3 (create a model)
+  const User = model<IUser>('User', userSchema);
+  
+  const createUserToDB = async () => {
+    const user = new User(
+      {
+        id: 'g-4-6969374',
+        role: 'student',
+        password: 'strong-password',
+        name: {
+          firstName: 'GL',
+          middleName: 'Abs',
+          lastName: "John"
+        },
+        dateOfBirth: '08 July 1996',
+        gender: 'male',
+        email: 'example@gmail.com',
+        contactNo: '+54654521454524',
+        emergencyContactNo: '+54654521454524',
+        presentAddress: 'Washington',
+        permanentAddress: 'California',
+      }
+    );
+  
+    await user.save();
+  }
+
+  createUserToDB();
+
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import app from './app';
+import app from "./app";
 
 // Port
 const port: string | number = process.env.PORT || 5000;
 
 // Database connection
 async function main() {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/test');
-        console.log('Successfully database connected!');
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("Successfully database connected!");
 
-        // Start the server
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    } catch (error) {
-        console.log('Failed to connect database', error);
-    }
-};
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to connect database", error);
+  }
+}
 
 main();
